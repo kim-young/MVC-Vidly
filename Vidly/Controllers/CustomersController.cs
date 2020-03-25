e0c0bb64d100ea7754eb7d5f91988279403a0c17
Young Kim <younghyun87@gmail.com>
2020-03-25 16:42:42 -0400

    Initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = GetCustomers();
+
+            return View(customer);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = GetCustomers().FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Mary Williams"},
+            };
+        }
+    }
+}
